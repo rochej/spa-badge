6440ac6f25b0082f21aecf592b722b074cfd1c07
Dave Spivey & roche <pair+davespivey.rochej@devbootcamp.com>
2016-03-08 12:32:01 -0800

    Celebrate Handlebars

--- api/app/controllers/peeps_controller.rb
+++ api/app/controllers/peeps_controller.rb
@@ -3,6 +3,13 @@ class PeepsController < ApplicationController
 
   def index
     @peeps = Peep.all
+    @peeps_hash = {}
+    @peeps_hash[:peeps]=@peeps
+    render :json => @peeps_hash
+  end
+
+  def show
+    @badges = @peep.badges
   end
 
 end
--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Peep.delete_all
+Badge.delete_all
+Vote.delete_all
+
+@peep = Peep.create(name: "Dave")
+@peep.badges.create(text: "WAWGTN")
+@peep.badges.first.votes.create(up: true)
+
+@peep = Peep.create(name: "Roche")
+@peep.badges.create(text: "Most likely to pirouette.")
+@peep.badges.first.votes.create(up: true)
