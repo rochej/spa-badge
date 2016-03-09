6ae575cffbf5674b374ac72ad84315a5580ac77b
Dave Spivey & roche <pair+davespivey.rochej@devbootcamp.com>
2016-03-08 20:13:57 -0800

    Associate badge with peep

--- api/app/controllers/badges_controller.rb
+++ api/app/controllers/badges_controller.rb
@@ -6,7 +6,9 @@ class BadgesController < ApplicationController
     @peep = Peep.find(params[:peep_id])
     p "*" * 80
     p params
-    @badge = Badge.create(text: params[:text])
+    @badge = Badge.new(text: params[:text])
+    @badge.peep = @peep
+    @badge.save
 
     render :json => jsonify_badge(@peep, @badge)
   end
